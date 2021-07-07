9739b6dd40dfc31976d499745da8c8a5cc4f1306
Erik Wallin <walline@alvis1.int.private>
2021-07-07 22:49:49 +0200

    new file

---
+++ cifar10.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torchvision
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+class SNGP_CNN(nn.Module):
+
+    def __init__(self):
+
+        super(SNGP_CNN, self).__init__()
+
+
+
+    def forward(self, x):
+
+        
+
+    
+
+
+
+class SNGP():
+
+    def __init__():
+
+        
+    def forward(self, x):
+        
+    
+    
+    def train(self):
+
+        for 
+
+
+
+
+def main():
+    
+    pass
+
+
+
+if __name__ == "__main__":
+
+    flags.DEFINE_float("lr", 0.03, "Learning rate")
+    flags.DEFINE_float("wd", 0.0005, "Weight decay")
+    flags.DEFINE_integer("batch", 64, "Batch size")
+    flags.DEFINE_integer("epochs", 100, "Number of training epochs")
+    
+    app.run(main)
+    
